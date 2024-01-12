3ee7d5604ef456f7ef85ab65547716a3f57686d5
Xander <xanderpalmer7@gmail.com>
2024-01-11 21:19:18 -0500

    Enabled XMTP on Play component

--- src/components/pages/Play.jsx
+++ src/components/pages/Play.jsx
@@ -1,16 +1,68 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useSendMessage, useStartConversation, useStreamAllMessages } from "@xmtp/react-sdk"
+import { useSSX } from "@spruceid/ssx-react"
 
 import Header from "../Header"
 import Footer from "../Footer"
-import Board from "../GameBoard"
-import { createInitialBoard } from '../../tools/tools/board'
-import { PIECE_COLORS } from "../../tools/enums"
-import OpponentMessage from "../OpponentMessage"
-import PlayerMessage from "../PlayerMessage"
-import MessageInput from "../MessageInput"
+import { CONNECT_STATUS, GAME_STATUS, PIECE_COLORS } from "../../utils/enum"
+import { generateInitalMoves } from "../../utils/game/message"
+import MessageBoard from "../MessageBoard"
+import GameContainer from "../GameContainer"
+import { getContent } from "../../utils/message/message"
 
 function Play() {
-    const [board, setBoard] = useState(createInitialBoard())
+    const { startConversation } = useStartConversation();
+    const { sendMessage } = useSendMessage();
+    const [conversation, setConversation] = useState(null);
+    const { ssx } = useSSX();
+    const opponent = ssx.address() === '0x99FD46b167B0FBB8aC6f79E6f575A6199c2cb536' ? '0x7b653a3BA395275c2e4cB55Dd45Fc041A4074747' : '0x99FD46b167B0FBB8aC6f79E6f575A6199c2cb536'
+    const color = ssx.address() === '0x99FD46b167B0FBB8aC6f79E6f575A6199c2cb536' ? PIECE_COLORS.WHITE : PIECE_COLORS.BLACK;
+
+    useStreamAllMessages((message) => console.log("NEW MESSAGE", message))
+
+    const hash = 'abcdf'
+
+    const loadConvo = async () => {
+        const convo = await startConversation(opponent)
+
+        console.log("CONVO:", convo)
+        setConversation(convo.cachedConversation);
+    }
+
+    useEffect(() => {
+        loadConvo();
+    }, [])
+
+    const [moveNeg1, move0] = generateInitalMoves();
+    const [status, setStatus] = useState(CONNECT_STATUS.ACCEPT);
+    const [lastMove, setLastMove] = useState(moveNeg1);
+    const [currMove, setCurrMove] = useState(move0);
+    const sendMove = (nextMove) => {
+        setLastMove(currMove);
+        setCurrMove(nextMove);
+
+        console.log(currMove, nextMove)
+
+        sendMessage(conversation, `${hash}-${nextMove}`)
+    }
+
+    const getOpponentMove = (first, second) => {
+        console.log(first, second);
+        const { content: fContent } = first;
+        const ready = true;
+
+        if (ready && getContent(fContent) !== currMove.content) {
+            if (second) {
+                const { content: sContent } = second;
+
+                setLastMove(getContent(sContent));
+                setCurrMove(getContent(fContent));
+            } else {
+                setLastMove(currMove);
+                setCurrMove(getContent(fContent));
+            }
+        }
+    }
 
     return (
         <div className="min-h-screen bg-foreground grid grid-rows-[auto_1fr_auto]">
@@ -18,82 +70,16 @@ function Play() {
             <div className="mt-4">
                 <div className="flex h-full flex-col xl:flex-row space-y-4 xl:space-y-0">
                     <div className="w-[100%] md:w-[75%] xl:w-1/2 bg-foreground mx-auto">
-                        <div className="w-[85%] md:w-[85%] xl:w-[75%] h-full mx-auto">
-                            <Board board={board} player={PIECE_COLORS.BLACK} setBoard={setBoard} />
-                        </div>
+                        <GameContainer
+                            connectStatus={status}
+                            currMove={currMove}
+                            lastMove={lastMove}
+                            player={color}
+                            sendMove={sendMove}
+                        />
                     </div>
                     <div className="w-[100%] md:w-[75%] xl:w-1/2 mx-auto h-[450px] md:h-[600px] xl:h-[90%]">
-                        <div className="w-[85%] md:w-[85%] xl:w-[75%] h-full mx-auto flex flex-col justify-around items-center bg-[#68a239] rounded-xl">
-                            <div className="bg-[#70c729] h-[80%] w-[95%] rounded-xl px-4 py-2">
-                                <div className="border-b border-white mb-4 flex justify-center items-center text-2xl h-[10%]">
-                                    Opponent.eth
-                                </div>
-                                <div className="overflow-y-scroll flex justify-start flex-col h-[85%] textbar space-y-2">
-                                    <OpponentMessage message={'A'} newThread={true} profileSrc={'https://noun-api.com/beta/pfp?name=A'} />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={true}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <PlayerMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=B'}
-                                    />
-                                    <OpponentMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={true}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=A'}
-                                    />
-                                    <OpponentMessage
-                                        message={'ABCDEFG ABCDEFG ABCDEFG ABCDEFG'}
-                                        newThread={false}
-                                        profileSrc={'https://noun-api.com/beta/pfp?name=A'}
-                                    />
-                                </div>
-                            </div>
-                            <MessageInput />
-                        </div>
+                        {conversation ? <MessageBoard hash={hash} conversation={conversation} playerAddr={ssx.address()} sendGameDetails={getOpponentMove} /> : null}
                     </div>
                 </div>
             </div>
