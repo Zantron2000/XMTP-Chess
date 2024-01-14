158759b0ced37adb08712b3a01ad960217544941
Xander <xanderpalmer7@gmail.com>
2024-01-13 23:29:49 -0500

    Updated GameSquares to prepare for pre-loaded boards

--- src/components/GameSquare.jsx
+++ src/components/GameSquare.jsx
@@ -1,14 +1,14 @@
 import { getImageClass } from "../tools/tools/piece";
 
 function GameSquare({ details, manager, position, makeMove, setSelected }) {
-    const divStyle = details.selectable && details.action ? `div-${details.action[2]}` : 'hidden';
+    const divStyle = details?.selectable && details?.action ? `div-${details.action[2]}` : 'hidden';
     const checkerBackground = manager.getTileBackground(position) === 1 ? 'bg-checker1' : 'bg-checker2';
     const pieceStatus = manager.isSelectedPiece(position) ? 'bg-selected-piece-background' : 'border-white/0';
-    const imageClass = getImageClass(details.piece);
+    const imageClass = getImageClass(details?.piece);
     let action = undefined;
 
-    if (details.selectable) {
-        if (details.action) {
+    if (details?.selectable) {
+        if (details?.action) {
             action = () => manager.executeAction(details.action, undefined, makeMove);
         } else {
             action = () => manager.toggleTile(position, setSelected);
@@ -18,7 +18,7 @@ function GameSquare({ details, manager, position, makeMove, setSelected }) {
     return (
         <button
             className={`border ${checkerBackground} ${pieceStatus} ${imageClass} flex items-center justify-center`}
-            disabled={!details.selectable}
+            disabled={!details?.selectable}
             onClick={action}
         >
             <div className={`rounded-full w-1/2 h-1/2 ${divStyle}`}></div>
