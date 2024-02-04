9d68dff0544ec48443018a46e262939af5581320
Xander <xanderpalmer7@gmail.com>
2024-02-03 23:21:41 -0500

    Expanded load history to work with loading games

--- src/utils/message/message.js
+++ src/utils/message/message.js
@@ -1,4 +1,5 @@
 import { CONNECT_STATUS, DEV_MODE, MESSAGE } from "../enum";
+import { generateInitalMoves } from "../game/message";
 import { getEnemyColor } from "../game/piece";
 
 export const isHashContent = (content, hash) => {
@@ -95,7 +96,37 @@ export const findMostRecentGameMessages = (messages, hash) => {
 
     return gameMessages;
 }
-
+/**
+ * Searches through history of messages to find the most recent game history
+ * Under the assumption that the last message is the most recent. The logic is as follows:
+ * Search for the most recent invite, accept and existing game. A game counts as existing if
+ * it has at least an invite and accept, and any number of moves. Only one game can exist at a
+ * time, and an existing game removes all existing invites and accepts that came before it.
+ * Invites from both players are only valid if they come before an accepted game, and have not
+ * recieved a response. Any invalid invites and games should be ignored, such as if both most
+ * recent game moves are from the same player, or any status message is sent to an invite
+ * 
+ * @param {import('@xmtp/react-sdk').Message[]} messages The histor of messages to search through
+ * @param {String} playerAddr The address of the player
+ * @returns {{
+ *  invite?: {
+ *      hash: String,
+ *      color: String
+ *  },
+ *  accept?: {
+ *      hash: String,
+ *      color: String
+ *  },
+ *  load?: {
+ *      lastMove: String,
+ *      currMove: String,
+ *      hash: String,
+ *      color: String
+ *  }
+ * }} The most recent game history, with the most recent invite from the player, the most recent 
+ * invite from the opponent, and the most recent game found. If no history is found, the value will
+ * be null.
+ */
 export const loadGameHistory = (messages, playerAddr) => {
     const data = {
         invite: {
@@ -106,11 +137,19 @@ export const loadGameHistory = (messages, playerAddr) => {
             hash: null,
             color: undefined,
         },
+        load: {
+            lastMove: null,
+            currMove: null,
+            playerWent: null,
+            hash: null,
+            color: undefined,
+        }
     }
 
     const searching = {
         invite: true,
         accept: true,
+        load: true,
     }
 
     const invalidHashes = [];
@@ -133,6 +172,28 @@ export const loadGameHistory = (messages, playerAddr) => {
         if (isGameContent(content)) {
             searching.invite = false;
             searching.accept = false;
+            if (searching.load) {
+                if (data.load.hash === hash && data.load.playerWent !== playerMessage) {
+                    const determinedColor = gameContent.split(MESSAGE.GAME_DELIMITER)[1];
+                    if ((playerMessage && determinedColor === data.load.color) || (!playerMessage && determinedColor !== getEnemyColor(data.load.color))) {
+                        data.load.lastMove = gameContent;
+                        searching.load = false;
+                    } else {
+                        invalidHashes.push(hash);
+                        searching.load = false;
+                    }
+                } else if (data.load.hash === null) {
+                    const color = gameContent.split(MESSAGE.GAME_DELIMITER)[1];
+                    data.load.hash = hash;
+                    data.load.currMove = gameContent;
+                    data.load.playerWent = playerMessage;
+                    data.load.color = playerMessage ? color : getEnemyColor(color);
+                } else {
+                    searching.load = false;
+                }
+            }
+
+            continue;
         }
 
         const status = gameContent.split(MESSAGE.GAME_DELIMITER)[0];
@@ -145,16 +206,84 @@ export const loadGameHistory = (messages, playerAddr) => {
                 data.accept.hash = hash;
                 data.accept.color = getEnemyColor(gameContent.split(MESSAGE.GAME_DELIMITER)[1]);
                 searching.accept = false;
+            } else if (searching.load && data.load.hash === hash && !invalidHashes.includes(hash)) {
+                if (playerMessage !== data.load.playerWent && data.load.currMove.startsWith(CONNECT_STATUS.ACCEPT + MESSAGE.GAME_DELIMITER)) {
+                    const determinedColor = gameContent.split(MESSAGE.GAME_DELIMITER)[1];
+                    if ((playerMessage && determinedColor === data.load.color) || (!playerMessage && determinedColor !== getEnemyColor(data.load.color))) {
+                        data.load.lastMove = gameContent;
+                        searching.load = false;
+                    } else {
+                        invalidHashes.push(hash);
+                        searching.load = false;
+                    }
+                } else {
+                    searching.load = false;
+                    invalidHashes.push(hash);
+                }
             }
         } else {
-            if (playerMessage && searching.accept) {
-                invalidHashes.push(hash);
+            if (status === CONNECT_STATUS.ACCEPT && searching.load) {
+                if (data.load.hash === hash) {
+                    const determinedColor = gameContent.split(MESSAGE.GAME_DELIMITER)[1];
+                    if ((playerMessage && determinedColor === data.load.color) || (!playerMessage && determinedColor !== getEnemyColor(data.load.color))) {
+                        data.load.lastMove = gameContent;
+                        searching.load = false;
+                    } else {
+                        invalidHashes.push(hash);
+                        searching.load = false;
+                    }
+                } else if (data.load.hash === null) {
+                    data.load.currMove = gameContent;
+                    data.load.hash = hash;
+                    data.load.hash = hash;
+                    data.load.playerWent = playerMessage;
+                    data.load.color = playerMessage ? gameContent.split(MESSAGE.GAME_DELIMITER)[1] : getEnemyColor(gameContent.split(MESSAGE.GAME_DELIMITER)[1]);
+                }
+
                 searching.accept = false;
-            } else if (!playerMessage && searching.invite) {
-                invalidHashes.push(hash);
                 searching.invite = false;
+            } else if (status === CONNECT_STATUS.DECLINE) {
+                if (playerMessage && searching.accept) {
+                    invalidHashes.push(hash);
+                    searching.accept = false;
+                } else if (!playerMessage && searching.invite) {
+                    invalidHashes.push(hash);
+                    searching.invite = false;
+                }
+            } else {
+                searching.accept = false;
+                searching.invite = false;
+                searching.load = false;
             }
         }
+    }
+
+    const [moveNeg1, move0] = generateInitalMoves();
+
+    if (data.load.lastMove && data.load.currMove) {
+        if (!isGameContent(data.load.lastMove) && !isGameContent(data.load.currMove)) {
+            data.load.lastMove = moveNeg1;
+            data.load.currMove = move0;
+        } else if (!isGameContent(data.load.lastMove) && isGameContent(data.load.currMove)) {
+            data.load.lastMove = move0;
+        }
+
+        delete data.load.playerWent;
+    } else {
+        data.load.lastMove = null;
+        data.load.currMove = null;
+        data.load.hash = null;
+    }
+    delete data.load.playerWent;
+
+    if (!data.invite.hash) {
+        data.invite = null;
+    }
+    if (!data.accept.hash) {
+        data.accept = null;
+    }
+    if (!data.load.hash) {
+        data.load = null;
     }
 
     return data;
