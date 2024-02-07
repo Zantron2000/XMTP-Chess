1ec03c7327396cb403f91d84fb768bed632885c0
Xander <xanderpalmer7@gmail.com>
2024-02-06 22:29:47 -0500

    Added transformation pop up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,25 +2,47 @@ import { useState, useEffect } from 'react';
 
 import GameSquare from './GameSquare';
 import BoardManager from '../utils/managers/BoardManager';
-import { INDEX_TO_COL, INDEX_TO_ROW, PIECE_COLORS, GAME_STATUS } from '../utils/enum';
+import { INDEX_TO_COL, INDEX_TO_ROW, PIECE_COLORS, GAME_STATUS, PIECE_VALUES } from '../utils/enum';
+import TransformButton from './TransformButton';
 
 function Board({ player, status, setStatus, lastMove, currMove, sendMove, gameOver, endGame }) {
     const [selectedTile, setSelectedTile] = useState(undefined);
     const [boardDetails, setBoardDetails] = useState({});
-    const boardManager = new BoardManager(lastMove, currMove, selectedTile, status, player, gameOver);
-
-    boardManager.getStatus(setStatus, (data) => console.log(data), endGame);
+    const [enPassant, setEnPassant] = useState(undefined);
+    const [transform, setTransform] = useState(undefined);
+    const tempSet = (data) => {
+        if (data !== enPassant) {
+            setEnPassant(data);
+        }
+    }
+    const [boardManager, setBoardManager] = useState(new BoardManager(lastMove, currMove, selectedTile, status, player, gameOver, enPassant));
 
     const squares = [];
     const { rowLabels, colLabels } = boardManager.getLabelOrder();
-    const makeMove = (nextMove) => {
+    const makeMove = (nextMove, transformingPawn) => {
         setSelectedTile(undefined);
+        boardManager.setSelectedTile(undefined);
 
-        sendMove(nextMove);
+        console.log('transformingPawn', transformingPawn)
+        if (transformingPawn) {
+            setTransform(() => (pieceType) => {
+                console.log("Beforehand", nextMove)
+                const transformMove = nextMove.replace(transformingPawn, pieceType + transformingPawn);
+                console.log('Now transforming', transformMove)
+
+                sendMove(transformMove);
+                setTransform(undefined);
+            });
+        } else {
+            console.log('Just sending move', nextMove)
+            sendMove(nextMove);
+        }
     }
 
     useEffect(() => {
-        boardManager.getStatus(setStatus, (data) => console.log(data), endGame);
+        boardManager.setLastMove(lastMove);
+        boardManager.setCurrMove(currMove);
+        boardManager.getStatus(setStatus, (data) => console.log(data), endGame, tempSet);
 
         if (boardManager.status !== GAME_STATUS.CHEAT) {
             const details = boardManager.getBoardDetails();
@@ -29,7 +51,17 @@ function Board({ player, status, setStatus, lastMove, currMove, sendMove, gameOv
         } else {
             setBoardDetails({});
         }
-    }, [currMove, selectedTile]);
+    }, [currMove]);
+
+    useEffect(() => {
+        if (boardManager.status !== GAME_STATUS.CHEAT) {
+            const details = boardManager.getBoardDetails();
+
+            setBoardDetails(details);
+        } else {
+            setBoardDetails({});
+        }
+    }, [selectedTile]);
 
     for (let row = 0; row < 8; row += 1) {
         for (let col = 7; col > -1; col -= 1) {
@@ -54,13 +86,21 @@ function Board({ player, status, setStatus, lastMove, currMove, sendMove, gameOv
 
     return (
         <div className='w-full'>
-            <div className='w-full bg-[#65a92d] rounded-xl'>
+            <div className='w-full bg-[#65a92d] rounded-xl relative'>
                 <div className='grid grid-cols-[auto_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_auto] text-center'>
                     <div className='invisible px-2'>X</div>
                     {colLabels.map((label) => <div className='py-2' key={label}>{label}</div>)}
                     <div className='invisible px-2'>X</div>
                 </div>
                 <div className='flex w-full'>
+                    {
+                        transform ? <div className='absolute w-full h-full top-0 rounded-xl grid grid-rows-2 grid-cols-2'>
+                            <TransformButton position={'top-left'} type={PIECE_VALUES.QUEEN} color={player} transform={transform} />
+                            <TransformButton position={'top-right'} type={PIECE_VALUES.ROOK} color={player} transform={transform} />
+                            <TransformButton position={'bottom-left'} type={PIECE_VALUES.KNIGHT} color={player} transform={transform} />
+                            <TransformButton position={'bottom-right'} type={PIECE_VALUES.BISHOP} color={player} transform={transform} />
+                        </div> : null
+                    }
                     <div>
                         <div className='h-full grid grid-rows-[8] text-center'>
                             {rowLabels.map((label) => <div key={label} className='px-2 flex items-center'>{label}</div>)}
--- src/components/GameSquare.jsx
+++ src/components/GameSquare.jsx
@@ -4,7 +4,7 @@ function GameSquare({ details, manager, position, makeMove, setSelected }) {
     const divStyle = details?.selectable && details?.action ? `div-${details.action[2]}` : 'hidden';
     const checkerBackground = manager.getTileBackground(position) === 1 ? 'bg-checker1' : 'bg-checker2';
     const pieceStatus = manager.isSelectedPiece(position) ? 'bg-selected-piece-background' : 'border-white/0';
-    const imageClass = getImageClass(details?.piece);
+    const imageClass = manager.getImageClass(details?.piece)
     let action = undefined;
 
     if (details?.selectable) {
