44f7d90cf7c0bb1031ceef740b5dd64d76de76b3
Xander <xanderpalmer7@gmail.com>
2024-02-10 22:50:55 -0500

    Documented message functions

--- src/components/GameSquare.jsx
+++ src/components/GameSquare.jsx
@@ -1,5 +1,3 @@
-import { getImageClass } from "../tools/tools/piece";
-
 function GameSquare({ details, manager, position, makeMove, setSelected }) {
     const divStyle = details?.selectable && details?.action ? `div-${details.action[2]}` : 'hidden';
     const checkerBackground = manager.getTileBackground(position) === 1 ? 'bg-checker1' : 'bg-checker2';
@@ -9,7 +7,7 @@ function GameSquare({ details, manager, position, makeMove, setSelected }) {
 
     if (details?.selectable) {
         if (details?.action) {
-            action = () => manager.executeAction(details.action, undefined, makeMove);
+            action = () => manager.executeAction(details.action, makeMove);
         } else {
             action = () => manager.toggleTile(position, setSelected);
         }
--- src/utils/game/message.js
+++ src/utils/game/message.js
@@ -7,6 +7,12 @@ const pawnTypes = PIECE_VALUES.QUEEN + PIECE_VALUES.ROOK + PIECE_VALUES.BISHOP +
 const validRows = "ABCDEFGH";
 const validCols = "12345678";
 
+/**
+ * Translates a message canCastle property to a boolean
+ * 
+ * @param {String} rookCanCastle The value of the rook's canCastle property
+ * @returns {Boolean | undefined} Whether or not the rook can castle
+ */
 const translateCastleInfo = (rookCanCastle) => {
     if (rookCanCastle === MESSAGE.TRUE) {
         return true;
@@ -25,14 +31,10 @@ const translateCastleInfo = (rookCanCastle) => {
  *  board: String, 
  *  player: PIECE_COLORS[keyof PIECE_COLORS],
  *  canCastle: {
- *     w: {
- *          1: Boolean,
- *          2: Boolean,
- *     },
- *     b: {
- *          1: Boolean,
- *          2: Boolean,
- *     },
+ *     [PIECE_COLORS[keyof PIECE_COLORS]]: {
+ *       1: Boolean | undefined,
+ *       2: Boolean | undefined,
+ *     }
  *   },
  * }}
  */
@@ -95,12 +97,38 @@ export const getNextTurn = (move) => {
     return isWhite(player) ? GAME_STATUS.BLACK_TURN : GAME_STATUS.WHITE_TURN;
 }
 
+/**
+ * Checks to see if a board length is too long or too short
+ * 
+ * @param {String} board The board to check
+ * @returns {Boolean} Whether or not the board length is valid
+ */
 const validBoardLength = (board) => {
     return 64 <= board.length && board.length <= 80;
 };
 
-const hasSameColor = ({ player: lastPlayer }, { player: currentPlayer }) => PIECE_COLORS.isAlly(lastPlayer, currentPlayer);
-
+/**
+ * Checks to see if a player has reenabled castling. Re-enabling castling is when a player 
+ * loses the ability to castle, but then regains it
+ *
+ * @param {{
+ *   canCastle: {
+ *     [PIECE_COLORS[keyof PIECE_COLORS]]: {
+ *       1: Boolean | undefined,
+ *       2: Boolean | undefined,
+ *     }
+ *   }
+ * }} currTurnDetails Details about the turn 
+ * @param {{
+ *   canCastle: {
+*     [PIECE_COLORS[keyof PIECE_COLORS]]: {
+*       1: Boolean | undefined,
+*       2: Boolean | undefined,
+*     }
+*   }
+* }} lastTurnDetails Details about the turn
+ * @returns {Boolean} Whether or not the player has reenabled castling
+ */
 const hasReverseCastleEnable = ({ canCastle: lastCanCastle }, { canCastle: currentCanCastle }) => {
     const lastWhiteRook1 = lastCanCastle[PIECE_COLORS.WHITE][1];
     const lastWhiteRook2 = lastCanCastle[PIECE_COLORS.WHITE][2];
@@ -119,6 +147,12 @@ const hasReverseCastleEnable = ({ canCastle: lastCanCastle }, { canCastle: curre
     );
 };
 
+/**
+ * Checks to see if a pawn is in a transformed position
+ * 
+ * @param {import("../../types").Piece} pawn The pawn to check
+ * @returns {Boolean} Whether or not the pawn is in a transformed position
+ */
 const isTransformedPawnPos = (pawn) => {
     const hasPawnType = pawnTypes.includes(pawn[0]);
     const hasValidRow = validRows.includes(pawn[1]);
@@ -127,29 +161,50 @@ const isTransformedPawnPos = (pawn) => {
     return hasPawnType && hasValidRow && hasValidCol;
 }
 
-const isPiecePos = (piece) => {
-    const hasValidRow = validRows.includes(piece[0]);
-    const hasValidCol = validCols.includes(piece[1]);
-    const isDead = piece === 'XX'
+/**
+ * Checks to see if a piece is in a valid position
+ * 
+ * @param {import("../../types").ChessPos} chessPos The piece to check
+ * @returns {Boolean} Whether or not the piece is in a valid position
+ */
+const isPiecePos = (chessPos) => {
+    const hasValidRow = validRows.includes(chessPos[0]);
+    const hasValidCol = validCols.includes(chessPos[1]);
+    const isDead = chessPos === 'XX'
 
     return (hasValidRow && hasValidCol) || isDead;
 }
 
+/**
+ * Extracts the board information from a board string
+ * 
+ * @param {String} board The board to extract information from
+ * @param {String} blame The player to blame for the error
+ * @returns {{
+ *   piecePositions: import("../../types").Positions,
+ *   pawnRegistry: import("../../types").PawnRegistry,
+ *   error: String,
+ * }} The extracted board information
+ */
 const extractBoardInfo = (board, blame = '') => {
     const piecePositions = {};
     const pawnRegistry = {};
     let pieceCount = 0;
     let transformCount = 0;
 
+    // Validates the board length
     if (!validBoardLength(board)) {
         return {
             error: GAME_VALIDATION_MESSAGES.formatMessage(GAME_VALIDATION_MESSAGES.INVALID_BOARD_SIZE, blame)
         };
     }
 
+    // Goes through the board and extracts the piece positions
     for (let i = 0; i < board.length; i += 2) {
+        // Gets the expected piece
         const piece = PIECE_MESSAGE_ORDER[pieceCount];
 
+        // Checks to see if the piece is in a transformed position or a regular position
         if (isPiece(piece, PIECE_VALUES.PAWN) && isTransformedPawnPos(board.substring(i, i + 3))) {
             pawnRegistry[piece] = board.substring(i, i + 1);
             piecePositions[piece] = board.substring(i + 1, i + 3);
@@ -166,11 +221,14 @@ const extractBoardInfo = (board, blame = '') => {
         }
     }
 
+    // There should be exactly 32 pieces on the board
     if (pieceCount !== 32) {
         return {
             error: GAME_VALIDATION_MESSAGES.formatMessage(GAME_VALIDATION_MESSAGES.INVALID_PIECE_COUNT, blame)
         };
     }
+
+    // There can only be 16 transformed pawns at most
     if (transformCount > 16) {
         return {
             error: GAME_VALIDATION_MESSAGES.formatMessage(GAME_VALIDATION_MESSAGES.INVALID_TRANSFORM_COUNT, blame)
@@ -180,23 +238,58 @@ const extractBoardInfo = (board, blame = '') => {
     return { piecePositions, pawnRegistry };
 }
 
+/**
+ * Checks to see if the castle details are valid. Castle details are valid if they contain
+ * boolean values for each rook
+ *
+ * @param {{
+ *   [PIECE_COLORS[keyof PIECE_COLORS]]: {
+ *     1: {
+ *       [PIECE_COLORS[keyof PIECE_COLORS]]: String,
+ *     },
+ *     2: {
+ *       [PIECE_COLORS[keyof PIECE_COLORS]]: String,
+ *     }
+ *   }
+ * }} castleDetails The details about the castling
+ * @returns {Boolean} Whether or not the castle details are valid
+ */
 const isValidCastleDetails = (castleDetails) => {
     return Object.values(castleDetails).every((player) => {
         return player['1'] !== undefined && player['2'] !== undefined;
     })
 }
 
+/**
+ * Gets the differences between two game messages
+ * 
+ * @param {String} lastBoard The last board
+ * @param {String} currBoard The current board
+ * @returns {{
+ *   differences: {
+ *     [PIECE_VALUES[keyof PIECE_VALUES]]: [String, String],
+ *   },
+ *   currPositions: import("../../types").Positions | undefined,
+ *   lastPositions: import("../../types").Positions | undefined,
+ *   lastPawnRegistry: import("../../types").PawnRegistry | undefined,
+ *   currPawnRegistry: import("../../types").PawnRegistry | undefined,
+ *   transformed: Boolean | undefined,
+ *   error: String | undefined,
+ * }} The differences between the two game messages
+ */
 const getGameDifferences = (lastBoard, currBoard) => {
     const { piecePositions: lastPositions, error: lastError, pawnRegistry: lastPawnRegistry } = extractBoardInfo(lastBoard);
     const { piecePositions: currPositions, error: currError, pawnRegistry: currPawnRegistry } = extractBoardInfo(currBoard);
     const differences = {};
 
+    // Stops if there was an error extracting any board information
     if (lastError || currError) {
         return {
             error: lastError || currError
         };
     }
 
+    // Goes through each piece and checks to see if there was a difference
     PIECE_MESSAGE_ORDER.forEach((piece) => {
         const lastPos = lastPositions[piece];
         const currentPos = currPositions[piece];
@@ -213,10 +306,12 @@ const getGameDifferences = (lastBoard, currBoard) => {
     const currPawnKeys = Object.keys(currPawnRegistry);
     let transformed = false;
 
+    // Ensures that there was only at most one difference in the pawn registries
     if (Math.abs(lastPawnKeys.length - currPawnKeys.length) > 1) {
         return { error: GAME_VALIDATION_MESSAGES.INVALID_TRANSFORM_DIFFERENCE };
     }
 
+    // Checks to see if a pawn was transformed
     if (currPawnKeys.length - lastPawnKeys.length === 1) {
         transformed = true;
     }
@@ -224,14 +319,7 @@ const getGameDifferences = (lastBoard, currBoard) => {
     return { differences, currPositions, lastPositions, lastPawnRegistry, currPawnRegistry, transformed };
 }
 
-const playerCastled = (lastCastleDetails, currCastleDetails, player) => {
-    const lastRook1 = lastCastleDetails[player][1];
-    const lastRook2 = lastCastleDetails[player][2];
-    const currRook1 = currCastleDetails[player][1];
-    const currRook2 = currCastleDetails[player][2];
-
-    return (lastRook1 !== currRook1) || (lastRook2 !== currRook2);
-}
+const hasSameColor = ({ player: lastPlayer }, { player: currentPlayer }) => PIECE_COLORS.isAlly(lastPlayer, currentPlayer);
 
 export const validateTurnContinuity = (lastMove, currentMove) => {
     const lastMoveDetails = extractMoveDetails(lastMove);
@@ -306,12 +394,25 @@ export const validateTurnContinuity = (lastMove, currentMove) => {
     };
 };
 
+/**
+ * Checks to see if a player can castle with a specific rook
+ * 
+ * @param {import("../../types").GameMessage} move The move to extract details from
+ * @param {PIECE_COLORS[keyof PIECE_COLORS]} player The player to check
+ * @param {Number} rookNum The number of the rook to check
+ * @returns {Boolean | undefined} Whether or not the player can castle with the rook
+ */
 export const canCastle = (move, player, rookNum) => {
     const { canCastle } = extractMoveDetails(move);
 
     return canCastle[player][rookNum];
 }
 
+/**
+ * Generates the first initial moves of the game
+ * 
+ * @returns {import("../../types").GameMessage[]} The first initial moves of the game
+ */
 export const generateInitalMoves = () => {
     const negOnePositions = { ...INITIAL_BOARD_POSITIONS, [PIECES.BLACK_KNIGHT_1]: 'C6' }
     const canCastleDetails = {
